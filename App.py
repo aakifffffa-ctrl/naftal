1cbc50975464b4e0a1693b4c15b7a80cd90c6819
aakifffffa-ctrl <aakiffff.fa@gmail.com>
2026-09-06 04:09:49 +0200

    Update App.py

--- App.py
+++ App.py
@@ -40,9 +40,7 @@ html, body, [class*="css"] {
 </style>
 """
 
-st.markdown(css_code, unsafe_allow_html=True)
-    }
-    
+st.markdown(css_code, unsafe_allow_html=True)  
     .main-header {
         background: linear-gradient(135deg, #1e3c72 0%, #2a5298 100%);
         color: white;
